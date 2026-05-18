e770ac89aa0fd885863ffcda90666052b0a3da9b
Don Noel <78627833+donnoel@users.noreply.github.com>
2026-05-17 20:16:06 -0700

    Add Explore World Pulse view model

---
+++ Pulseboard/Features/Explore/ViewModels/ExploreWorldPulseViewModel.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+@MainActor
+final class ExploreWorldPulseViewModel: ObservableObject {
+    @Published private(set) var countryProfiles: [CountryPulseProfile] = []
+
+    private let countryProfileService: CountryPulseProfileService
+    private var hasLoaded = false
+
+    init(countryProfileService: CountryPulseProfileService = CountryPulseProfileService()) {
+        self.countryProfileService = countryProfileService
+    }
+
+    func loadIfNeeded() async {
+        guard !hasLoaded else {
+            return
+        }
+
+        hasLoaded = true
+        countryProfiles = await countryProfileService.fetchPreviewProfiles()
+    }
+}
